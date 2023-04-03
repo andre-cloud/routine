7d2a4e85ae7def59df9969522fdb18d69f8200c0
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2023-04-03 19:12:20 +0200

    Update write_job_file.py

--- src/write_job_file.py
+++ src/write_job_file.py
@@ -45,7 +45,7 @@ mv *.* qm_all/
 
 for i in ${"{ext[@]}"}; do 
     if [[ -e {input_file_no_extention}.$i ]]; then
-	    cp -v qm_all/{input_file_no_extention}.$i .;
+	    cp -v $SLURM_SUBMIT_DIR/qm_all/{input_file_no_extention}.$i $SLURM_SUBMIT_DIR;
     fi
 done
 '''
