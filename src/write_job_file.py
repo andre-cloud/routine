87f287043d5c165b23fb3cda7f72daae2e1bece1
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2024-01-12 11:55:22 +0100

    Update write_job_file.py

--- src/write_job_file.py
+++ src/write_job_file.py
@@ -66,15 +66,15 @@ def write_job_file(input_file, calculation, calc_cmd, slurm_cmd, command_1=None,
 
     output_file = OUTPUT_FILE[calculation].format(input=input_file_no_extention)
 
-
+    # defining the command line to run the code
     if calculation in ['orca',]:
-        cm = calc_cmd + f' > $SLURM_SUBMIT_DIR/{output_file}'
+        cm = calc_cmd + f' > {output_file}'
     elif calculation in ['gaussian']:
         cm = calc_cmd
     elif calculation in ['crest', 'censo', 'xtb']:
-        cm = calc_cmd + f' > $SLURM_SUBMIT_DIR/{output_file} 2> $SLURM_SUBMIT_DIR/{calculation}.error'
+        cm = calc_cmd + f' > {output_file} 2> {calculation}.error'
     elif calculation in ['enan']:
-        cm = calc_cmd + f' -o $SLURM_SUBMIT_DIR/{output_file}'
+        cm = calc_cmd + f' -o {output_file}'
 
 
     cmd_not_prog = ' '.join(cm.split()[1:])
