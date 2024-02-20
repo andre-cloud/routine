a84d33e99b1c038d1b643c786405a016b59c23cf
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2024-02-20 17:04:28 +0100

    update DNS mailer

--- src/defaults.py
+++ src/defaults.py
@@ -89,7 +89,7 @@ OUTPUT_FILE = {
     'enan'      : 'output.out',
 }
 
-SMTP_SERVER_IP = '10.147.18.66:8025'
+SMTP_SERVER_IP = 'mailrise.synlab.navile'
 DEFAULT_MAIL='AP_tgram@mailrise.xyz,PR_tgram@mailrise.xyz'
 
 
